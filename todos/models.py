3d788f756a44fb27d9e8f00feb189bec40acdfc1
josegill12 <jose.gill1988@gmail.com>
2023-10-25 16:39:56 -0600

    second post

--- todos/models.py
+++ todos/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class Todo(models.Model):
     subject = models.CharField(max_length=100)
     details = models.CharField(max_length=100)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    
+class Completed(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
